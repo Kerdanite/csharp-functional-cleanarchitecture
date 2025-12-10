20500c6a4f84855bd0c6ad7e5b74e174af1d222e
Joffrey TRUYEN <j-truyen@ddslogistics.com>
2025-12-10 11:36:15 +0100

    init: clean web project not necessary

--- AspireCsharpFunctional/AspireCsharpFunctional.AppHost/AppHost.cs
+++ AspireCsharpFunctional/AspireCsharpFunctional.AppHost/AppHost.cs
@@ -3,10 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var apiService = builder.AddProject<Projects.AspireCsharpFunctional_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
-builder.AddProject<Projects.AspireCsharpFunctional_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService);
+
 
 builder.Build().Run();
